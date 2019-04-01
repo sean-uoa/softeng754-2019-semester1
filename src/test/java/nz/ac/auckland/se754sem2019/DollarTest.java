653f567be31e6c0c2ccf19ca6f7326e2c9d8d0cc
Sean-gzcn <xyzhanggz@gmail.com>
2019-04-01 23:54:54 +1300

    add code and dependencies

---
+++ src/test/java/nz/ac/auckland/se754sem2019/DollarTest.java
@@ -0,0 +1,50 @@
+package nz.ac.auckland.se754sem2019;
+import static org.junit.Assert.*;
+
+import org.bson.Document;
+import org.junit.Test;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class DollarTest {
+	
+	@Test
+	public void testDollarDivision() {
+		// Given
+		Dollar ten = new Dollar(10);
+		
+		// When
+		Dollar result = ten.dividedBy(2);
+		
+		// Then
+		assertEquals(new Dollar(4), result);  // Should be 5
+	}
+	
+	@Test(expected=ArithmeticException.class)
+	public void testDollarDividedByZero() {
+		// Given
+		Dollar ten = new Dollar(10);
+		
+		// When
+		ten.dividedBy(0);
+	}
+	
+	@Test
+	public void testDollarPersistence() {
+		
+		MongoClient mongoClient = new MongoClient("localhost", 27017);
+		String myCashDatabaseName = "my-cash-db";
+		MongoDatabase mongoDatabase = mongoClient.getDatabase(myCashDatabaseName);
+		String myCashCollectionName = "my-cash-collection";
+		MongoCollection<Document> mongoCollection = mongoDatabase.getCollection(myCashCollectionName);
+		
+		Dollar five = new Dollar(5);
+		Document document = new Document();
+		document.put("NZD", five.toString());
+		mongoCollection.insertOne(document);
+		System.out.println("Dollar save action perfomed!");
+	}
+
+}
