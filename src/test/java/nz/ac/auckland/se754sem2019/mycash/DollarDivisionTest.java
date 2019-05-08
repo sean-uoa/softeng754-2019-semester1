30969566a07421a1ec41270caf348c4eabb23ff9
Sean-gzcn <xyzhanggz@gmail.com>
2019-05-09 00:35:08 +1200

    Add the devision test code

---
+++ src/test/java/nz/ac/auckland/se754sem2019/mycash/DollarDivisionTest.java
@@ -0,0 +1,20 @@
+package nz.ac.auckland.se754sem2019.mycash;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DollarDivisionTest {
+
+	@Test
+	public void testDollarDivision() {
+		// Given
+		Dollar ten = new Dollar(10);
+		
+		// When
+		Dollar result = ten.dividedBy(2);
+		
+		// Then
+		assertEquals(new Dollar(5), result);
+	}
+}
