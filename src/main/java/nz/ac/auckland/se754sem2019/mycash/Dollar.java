71aec201d06ab806d1a4d86c9d89291a003fdb7c
Sean-gzcn <xyzhanggz@gmail.com>
2019-05-08 23:49:32 +1200

    [R]Add access control modifier, leading compilation error in test code

--- src/main/java/nz/ac/auckland/se754sem2019/mycash/Dollar.java
+++ src/main/java/nz/ac/auckland/se754sem2019/mycash/Dollar.java
@@ -2,13 +2,13 @@ package nz.ac.auckland.se754sem2019.mycash;
 
 public class Dollar {
 
-	int amount;
+	private int amount;
 	
 	public Dollar(int amount) {
 		this.amount = amount;
 	}
 	
 	public void times(int multiplier) {
-		this.amount = this.amount*multiplier;
+		this.amount *= multiplier;
 	}
 }
