3c42f747158635996564635dc3602416af1c216a
Sean-gzcn <xyzhanggz@gmail.com>
2019-05-08 23:29:33 +1200

    Write a test and see the compilation errors

---
+++ src/test/java/nz/ac/auckland/se754sem2019/mycash/DollarMultiplicationTest.java
@@ -0,0 +1,20 @@
+package nz.ac.auckland.se754sem2019.mycash;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DollarMultiplicationTest {
+	
+	@Test
+	public void testMuliplication() {
+		// Preconditions
+		Dollar five = new Dollar(5);
+		
+		// Actions
+		five.times(2);
+		
+		// Postconditions
+		assertEquals(10, five.amount);
+	}
+}
