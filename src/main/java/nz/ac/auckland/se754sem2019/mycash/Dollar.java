975d3c5f97a59cb623f9ca7c2a74667ae18a7d3a
Sean-gzcn <xyzhanggz@gmail.com>
2019-05-09 23:04:24 +1200

    Add more tests and production code, Green

--- src/main/java/nz/ac/auckland/se754sem2019/mycash/Dollar.java
+++ src/main/java/nz/ac/auckland/se754sem2019/mycash/Dollar.java
@@ -4,14 +4,22 @@ public class Dollar {
 
 	private int amount;
 	private ICalculator calculator;
-	
+
 	public Dollar(int amount) {
 		this.amount = amount;
 	}
 	
 	public Dollar times(int multiplier) {
-		this.amount *= multiplier;
-		return this;
+		int resultantAmount = this.amount*multiplier;
+		return new Dollar(resultantAmount);
+	}
+	
+	public Dollar dividedBy(int denominator) throws NegativeNumberException{
+		if (denominator < 0) {
+			throw new NegativeNumberException();
+		}
+		int resultantAmount = this.amount/denominator;
+		return new Dollar(resultantAmount);
 	}
 	
 	@Override
@@ -27,27 +35,28 @@ public class Dollar {
 		return false;
 	}
 	
-	public Dollar dividedBy(int denominator) throws NegativeNumberException{
-		if (denominator < 0) {
-			throw new NegativeNumberException();
-		}
-		this.amount /= denominator;
-		return this;
-	}
-
 	public void setCalculator(ICalculator calculator) {
 		this.calculator = calculator;
 	}
-
+	
 	public boolean isCalculatorNull() {
 		return this.calculator == null;
 	}
-
+	
 	public Dollar timesWithCalculator(int multiplier) {
-		
+		if(multiplier < 0) {
+			throw new NegativeNumberException();
+		}
 		int resultantAmount = this.calculator.multiply(this.amount, multiplier);
 		return new Dollar(resultantAmount);
 	}
 	
+	public Dollar dividesWithCalculator(int denominator) {
+		if(denominator < 0) {
+			throw new NegativeNumberException();
+		}
+		int resultantAmount = this.calculator.divide(this.amount, denominator);
+		return new Dollar(resultantAmount);
+	}
 	
 }
--- src/main/java/nz/ac/auckland/se754sem2019/mycash/ICalculator.java
+++ src/main/java/nz/ac/auckland/se754sem2019/mycash/ICalculator.java
@@ -3,5 +3,7 @@ package nz.ac.auckland.se754sem2019.mycash;
 public interface ICalculator {
 
 	public int multiply(int multiplicand, int multiplier);
+	
+	public int divide(int dividend, int divisor);
 
 }
