cc38617833d3815dee1a8c6fbb23684ca93cffea
Sean-gzcn <xyzhanggz@gmail.com>
2019-05-10 00:58:10 +1200

    Add both test and production code for DAO DollarPersistence

---
+++ src/main/java/nz/ac/auckland/se754sem2019/mycash/DollarPersistence.java
@@ -0,0 +1,36 @@
+package nz.ac.auckland.se754sem2019.mycash;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class DollarPersistence {
+	
+	MongoClient mongoClient;
+	MongoDatabase mongoDatabase;
+	MongoCollection<Document> mongoCollection;
+	
+	public DollarPersistence(MongoClient mongoClient, 
+			String myCashDatabaseName, String myCashCollectionName) {
+		this.mongoClient = mongoClient;
+		this.mongoDatabase = this.mongoClient.getDatabase(myCashDatabaseName);
+		this.mongoCollection = this.mongoDatabase.getCollection(myCashCollectionName);
+	}
+	
+	public boolean isMongoDBClientNull() {
+		return this.mongoClient == null;
+	}
+	
+	public String getDBName() {
+		return this.mongoDatabase.getName();
+	}
+	
+	public void saveNZD(Dollar dollar) {
+		Document document = new Document();
+		document.put("NZD", dollar.toString());
+		this.mongoCollection.insertOne(document);
+	}
+	
+}
---
+++ src/test/java/nz/ac/auckland/se754sem2019/mycash/DollarPersistenceTest.java
@@ -0,0 +1,78 @@
+package nz.ac.auckland.se754sem2019.mycash;
+
+import static org.junit.Assert.*;
+
+import org.bson.Document;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class DollarPersistenceTest {
+	
+	MongoClient mongoClient;
+	String myCashDBName;
+	String myCashCollectionName;
+	MongoDatabase mongoDatabase;
+	
+	@Before
+	public void setUp() {
+		// Given
+		mongoClient = Mockito.mock(MongoClient.class);
+		myCashDBName = "my-cash-db";
+		myCashCollectionName = "my-cash-collection";
+		mongoDatabase =  Mockito.mock(MongoDatabase.class);
+	}
+
+	@Test
+	public void shouldInitializeMongoDBClientWhenCreatingMyCash() {
+		
+		// Given
+		Mockito.doReturn(mongoDatabase).when(mongoClient).getDatabase(myCashDBName);
+		
+		// When
+		DollarPersistence dollarPersistence = new DollarPersistence(mongoClient, myCashDBName, myCashCollectionName);
+		
+		// Then
+		assertFalse(dollarPersistence.isMongoDBClientNull());
+	}
+	
+	@Test
+	public void shouldHaveDatabaseWithANameAfterCreatingMyCash() {
+		
+		// Given
+		Mockito.doReturn(myCashDBName).when(mongoDatabase).getName();
+		Mockito.doReturn(mongoDatabase).when(mongoClient).getDatabase(myCashDBName);
+		
+		// When
+		DollarPersistence dollarPersistence = new DollarPersistence(mongoClient, myCashDBName, myCashCollectionName);
+		String dbName = dollarPersistence.getDBName();
+		
+		// Then
+		assertEquals(myCashDBName, dbName);
+	}
+	
+	@Test
+	public void shouldInvodeTheDatabaseSaveMethodWhenSavingDollar() {
+		// Given
+		Mockito.doReturn(mongoDatabase).when(mongoClient).getDatabase(myCashDBName);
+		
+		MongoCollection<Document> collection = Mockito.mock(MongoCollection.class);
+		Mockito.doReturn(collection).when(mongoDatabase).getCollection(myCashCollectionName);
+		
+		Dollar five = new Dollar(5);
+		Document document = new Document();
+		document.put("NZD", five.toString());
+		
+		// When
+		DollarPersistence dollarPersistence = new DollarPersistence(mongoClient, myCashDBName, myCashCollectionName);
+		dollarPersistence.saveNZD(five);
+		
+		// Then
+		Mockito.verify(collection, Mockito.times(1)).insertOne(document);
+	}
+
+}
