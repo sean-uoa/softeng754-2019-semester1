c564d48581435920937e90c5d60c30fc2c9dea02
Sean-gzcn <xyzhanggz@gmail.com>
2019-05-23 22:39:27 +1200

    Add Dollar. In real case, this involves TDD developing new code

---
+++ src/test/java/nz/ac/auckland/se754sem2019/mycash/acceptance/MultiplicationSteps.java
@@ -0,0 +1,30 @@
+package nz.ac.auckland.se754sem2019.mycash.acceptance;
+
+import static org.junit.Assert.assertEquals;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+import nz.ac.auckland.se754sem2019.mycash.Dollar;
+
+public class MultiplicationSteps {
+	
+	Dollar five;
+	Dollar ten;
+	
+	@Given("User has $amount dollars")
+	public void givenUserHas5Dollars(int amount) {
+		five = new Dollar(amount);
+	}
+
+	@When("User multiply it by $multiplier")
+	public void whenUserMultiplyItBy2(int multiplier) throws InterruptedException{
+		ten = five.times(multiplier);
+	}
+
+	@Then("The resultant amount is $result")
+	public void thenTheResultantAmountIs10(int result) {
+		assertEquals(new Dollar(result), ten);
+	}
+}
